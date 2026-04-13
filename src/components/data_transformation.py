83ab72a4f7c982489392924bb940d1c16420e689
Jatin475 <muk.jatin1234@gmail.com>
2026-04-13 18:40:50 +0530

    model Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -104,7 +104,6 @@ class DataTransformation:
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path
-
             )
              
         except Exception as e:
