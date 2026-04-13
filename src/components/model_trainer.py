1fe11cf4a0c9d039c55a9f4070e7d05ae44ca6c8
Jatin475 <muk.jatin1234@gmail.com>
2026-04-13 19:05:00 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,8 +45,34 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5, 7, 9, 11],
+                    'weights': ['uniform', 'distance']
+                },
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'depth': [6, 8, 10],
+                    'iterations': [30, 50, 100]
+                }   
+            }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models,param = params)
 
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,12 +19,19 @@ def save_object(file_path,obj):
     except Exception as e:
         return CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            grid_search_cv = GridSearchCV(model,para,cv=3)
+            grid_search_cv.fit(X_train,y_train)
+            model.set_params(**grid_search_cv.best_params_)
+
+
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -32,7 +40,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = (test_model_score,grid_search_cv.best_params_)
 
         return report
 
